708f65e0680fa17909f98aa497f13881f480f705
Simon Rozet <simon@rozet.name>
2014-05-13 18:35:19 +0200

    initial quotefixformac plugin

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,4 +1,51 @@
-# This is a placeholder class.
-class template {
-  anchor { 'Hello_World': }
+# Install the QuoteFix Mail.app plugin.
+#
+# Usage:
+#
+#   include quotefixformac
+class quotefixformac(
+  $version = '2.5.3'
+) {
+  $zipurl = "https://github.com/robertklep/quotefixformac/releases/download/\
+v${version}/QuoteFix-v${version}.zip"
+  $bundlesdir = "/Users/${::boxen_user}/Library/Mail/Bundles"
+  $zipfile = '/tmp/QuoteFix.zip'
+  $zipdest = "/tmp/QuoteFix-${version}"
+
+  boxen::osx_defaults { 'Enable Mail.app plugin support':
+    domain => 'com.apple.mail',
+    key    => 'EnableBundles',
+    type   => 'bool',
+    value  => true,
+  }
+
+  boxen::osx_defaults { 'Set Mail.app Bundle Compatibility Version':
+    domain => 'com.apple.mail',
+    key    => 'BundleCompatibilityVersion',
+    type   => 'string',
+    value  => 3,
+  }
+
+  file { $bundlesdir:
+    ensure => directory,
+  }
+
+  exec {
+    'quotefixformac-download':
+      command => "/usr/bin/curl -L ${zipurl} > '${zipfile}",
+      creates => $zipfile;
+    'quotefixformac-extract':
+      command     => "/usr/bin/unzip -d '${zipdest}' '${zipfile}'",
+      cwd         => '/tmp',
+      require     => Exec['quotefixformac-download'],
+      creates     => $zipdest,
+      refreshonly => true;
+  }
+
+  file { "${bundlesdir}/QuoteFix.mailbundle":
+    ensure  => present,
+    source  => "${zipdest}/QuoteFix.mailbundle",
+    replace => true,
+    require => Exec['quotefixformac-extract'],
+  }
 }
---
+++ spec/classes/quotefixformac_spec.rb
@@ -0,0 +1,57 @@
+require "spec_helper"
+
+describe "quotefixformac" do
+  let(:facts) { {:boxen_user => "sr"} }
+  let(:params) { {:version => "1" } }
+
+  it do
+    should contain_boxen__osx_defaults("Enable Mail.app plugin support").
+      with({
+      :domain => "com.apple.mail",
+      :key => "EnableBundles",
+      :type => "bool",
+      :value => "true"
+    })
+  end
+
+  it do
+    should contain_boxen__osx_defaults("Set Mail.app Bundle Compatibility Version").
+      with({
+        :domain => "com.apple.mail",
+        :key => "BundleCompatibilityVersion",
+        :type => "string",
+        :value => "3"
+    })
+  end
+
+  it do
+    should contain_file("/Users/sr/Library/Mail/Bundles").
+      with(:ensure => :directory)
+  end
+
+  it do
+    should contain_exec("quotefixformac-download").
+      with({
+        :creates => "/tmp/QuoteFix.zip",
+      })
+  end
+
+  it do
+    should contain_exec("quotefixformac-extract").
+      with({
+        :creates => "/tmp/QuoteFix-1",
+        :refreshonly => true,
+      }).
+      that_requires("Exec[quotefixformac-download]")
+  end
+
+  it do
+    should contain_file("/Users/sr/Library/Mail/Bundles/QuoteFix.mailbundle").
+      with({
+        :ensure => "present",
+        :source => "/tmp/QuoteFix-1/QuoteFix.mailbundle",
+        :replace => true,
+      }).
+      that_requires("Exec[quotefixformac-extract]")
+  end
+end
